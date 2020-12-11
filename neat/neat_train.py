1c59c662fed2010a9204f3a13e3c2a7d6d888894
Anthony Clemens <anthonysmacbook@Anthony.local>
2020-12-10 16:37:22 -0800

    version update

--- neat/neat_train.py
+++ neat/neat_train.py
@@ -6,11 +6,17 @@ import neat
 import sys
 import pickle
 
+# render flag, 0 off, 1 on, 0 trains faster
 render_flag = 0
+# number of generations
 num_generations = 10000
+# number of episodes per genome
 num_episodes = 10
+# max steps per episode
 max_steps = 200
 
+# input neural network, environment, number of episodes, 
+# number of steps, render flag, and outputs average fitness for that neural network (genome)
 def simulate_species(net, env, episodes, steps, render=False):
     fitnesses = []
     for runs in range(episodes):
@@ -32,30 +38,43 @@ def simulate_species(net, env, episodes, steps, render=False):
     print("Species fitness: %s" % str(fitness))
     return fitness
 
+# evaluate a genome
 def eval_genome(genome, config):
-    print(genome)
+    # create neural network
     net = neat.nn.feed_forward.FeedForwardNetwork.create(genome[1], config)
     return simulate_species(net, env, num_episodes, max_steps, render=render_flag)
 
+# assigns fitness to genome
 def eval_fitness(genomes, config):
     for genome in genomes:
         fitness = eval_genome(genome, config)
         genome[1].fitness = fitness
 
-def train_network(env):
+# train 
+def train(env):
+    # initialize config object
     config = neat.config.Config(genome_type=neat.genome.DefaultGenome, reproduction_type=neat.reproduction.DefaultReproduction, species_set_type=neat.species.DefaultSpeciesSet, stagnation_type=neat.stagnation.DefaultStagnation, filename='./DSI/reinforcement_learning/neat/neat_config.txt')
+    # initialize pop object
     pop = neat.population.Population(config)
+    # initialize stats object
     stats = neat.statistics.StatisticsReporter()
+    # add stats object to pop object
     pop.add_reporter(stats)
-
+    # run method
     pop.run(eval_fitness, num_generations)
+    # winner
     winner = stats.best_genome()
     
+    # save winner
     with open('./DSI/reinforcement_learning/assets/neat_winner.pkl', 'wb') as f:
        pickle.dump(winner, f)
 
+    # print winner
     print(f'Best genome:\n{winner}')
 
+# main
 if __name__ == '__main__':
+    # make environment
     env = gym.make('CartPole-v0')
-    train_network(env)
+    # train
+    train(env)
